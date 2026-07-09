c41b12e5e0880c71206491175e38fc16ebdf33c7
dhgaj30-jpg <dhgaj30@gmail.com>
2026-07-09 16:41:57 +0300

    Update dis.js

--- dis.js
+++ dis.js
@@ -139,7 +139,7 @@ async function scanAndAttack() {
         }
 
         // تحديد أحدث تعليقين فقط
-        const topComments = commentsList.slice(0, 10);
+        const topComments = commentsList.slice(0, 2);
 
         for (const comment of topComments) {
             const commentId = comment.anime_comment_id;
